43ecc87e0e755dfaef9f56b49ecee2a1cca7c4c4
Subin, Han <ella0411@naver.com>
2024-08-14 19:19:00 +0900

    08.14

---
+++ HW01-01/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
+import Header from "./components/Header";
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<Content />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
