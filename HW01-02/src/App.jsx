4b5ec2c0d3fa0c4c6939d4ad0df4ee356e4ddde8
Subin, Han <ella0411@naver.com>
2024-08-14 19:38:22 +0900

    08.14

---
+++ HW01-02/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
+function App() {
+	const title = `My Website`;
+	const year = 2024;
+
+	return (
+		<div>
+			<Header title={title} />
+			<Content />
+			<Footer year={year} />
+		</div>
+	);
+}
+
+export default App;
